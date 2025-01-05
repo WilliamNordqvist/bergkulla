75019e84bb5988db10e7953d39e510b52d869064
William Nordqvist <wnordqvist@gmail.com>
2025-01-05 13:56:31 +0100

    add Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -261,6 +262,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#FAF9F6]`}
       >
         <JsonLd />
+        <Analytics />
+
         {children}
       </body>
     </html>
